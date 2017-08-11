51b8ede6d3792e9e3ee31c8168179302d15e7ccc
Alex Park <apark0720@gmail.com>
2017-08-10 21:20:13 -0700

    create profile model and make migration

--- univentures/tags/models.py
+++ univentures/tags/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    profile = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.profile
