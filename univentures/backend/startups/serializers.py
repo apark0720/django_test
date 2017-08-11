7f1ac56d49d44ab8af7c382979ac25043736fe7d
Young Eun Kim <aakim0907@gmail.com>
2017-08-11 16:52:28 -0700

    define create and update class

--- univentures/backend/startups/serializers.py
+++ univentures/backend/startups/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
 from .models import Startup
 
-class StartupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Startup
-        fields = '__all__'
+class StartupSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=20)
+    email = serializers.CharField(max_length=30)
+    logo = serializers.FileField()
+    blurb = serializers.TextField(max_length=150)
+    description = serializers.TextField(max_length=1000)
+
+    def create(self, validated_data):
+        # create and return new Startup object, given the validated data
+        return Startup.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        # update and return existing Startup object, given the validated data
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.logo = validated_data.get('logo', instance.logo)
+        instance.blurb = validated_data.get('blurb', instance.blurb)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
