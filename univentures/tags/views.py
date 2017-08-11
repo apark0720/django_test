e722a0356902946510ca069680bf24c9239aa124
Alex Park <apark0720@gmail.com>
2017-08-10 21:46:16 -0700

    create profile views

--- univentures/tags/views.py
+++ univentures/tags/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Startup
+from .serializers import StartupSerializer
 
-# Create your views here.
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
