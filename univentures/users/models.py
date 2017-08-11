35596968a5041b036d7366ee4aed4b52f5120191
Alex Park <apark0720@gmail.com>
2017-08-10 20:54:07 -0700

    make migrations

--- univentures/users/models.py
+++ univentures/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
-    email = models.CharField()
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    email = models.CharField(max_length=20)
     profile_img = models.FileField()
     user_blurb = models.TextField(max_length=150)
-    
+
     def __str__(self):
         return self.id + ":" + self.first_name + " " + self.last_name
