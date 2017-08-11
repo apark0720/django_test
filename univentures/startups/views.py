3ac21ad029f0fd6b72e547349b7ef1a70b9f23de
Young Eun Kim <aakim0907@gmail.com>
2017-08-10 21:32:58 -0700

    create startup serializers and urls

--- univentures/startups/views.py
+++ univentures/startups/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Startup
+from .serializers import StartupSerializer
 
-# Create your views here.
+class StartupList(APIView):
+    def get(self, request):
+        startups = Startup.objects.all()
+        serializer = StartupSerializer(startups, many=True)
+        return Response(serializer.data)
+    
+    def post(self):
+        pass
--- univentures/univentures/urls.py
+++ univentures/univentures/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+from startups import views
+from tags import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^startups/', views.StartupList.as_view()),
+    url(r'profile/', views.ProfileList.as_view()),
 ]
