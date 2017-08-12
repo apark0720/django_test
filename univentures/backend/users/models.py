adbd899071cf5600ca10bbd2216f6648761bd497
Alex Park <apark0720@gmail.com>
2017-08-11 17:28:26 -0700

    add custom user model and update settings.py

--- univentures/backend/users/models.py
+++ univentures/backend/users/models.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _from django.db import models
 
-class User(models.Model):
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=15)
-    email = models.CharField(max_length=20)
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(_('email address'), unique=True)
     profile_img = models.FileField()
     user_blurb = models.TextField(max_length=150)
 
-    def __str__(self):
-        return self.id + ":" + self.first_name + " " + self.last_name
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
