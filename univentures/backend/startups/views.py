fab67798dbcb6d97386813046a489cf3ee83ab93
Young Eun Kim <aakim0907@gmail.com>
2017-08-11 17:43:45 -0700

    use generic class-based views

--- univentures/backend/startups/views.py
+++ univentures/backend/startups/views.py
@@ -1,49 +1,11 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Startup
 from .serializers import StartupSerializer
 
-class StartupList(APIView):
-    # List all Startups, or create a new Startup.
+class StartupList(generics.ListCreateAPIView):
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
 
-    def get(self, request, format=None):
-        startups = Startup.objects.all()
-        serializer = StartupSerializer(startups, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = StartupSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class StartupDetail(APIView):
-    # Retrieve, update or delete a startup instance.
-
-    def get_object(self, pk):
-        try:
-            return Startup.objects.get(pk=pk)
-        except Startup.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        startup = self.get_object(pk)
-        serializer = StartupSerializer(startup)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        startup = self.get_object(pk)
-        serializer = StartupSerializer(startup, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        startup = self.get_object(pk)
-        startup.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class StartupDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
