58285150c0876ab108495ba5c00f203d8002a2da
Alex Park <apark0720@gmail.com>
2017-08-11 22:56:54 -0700

    add store.js

--- univentures/frontend/store/store.js
+++ univentures/frontend/store/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+<<<<<<< f840553cf048d257a7a14bcf83977039c39c0cf7
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
@@ -9,12 +10,21 @@ if (process.env.NODE_ENV !== 'production') {
     const { createLogger } = require('redux-logger');
     middlewares.push(createLogger());
 }
+=======
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from '../reducers/root_reducer';
+>>>>>>> add store.js
 
 const configureStore = (preloadedState = {}) => (
     createStore(
         rootReducer,
         preloadedState,
+<<<<<<< f840553cf048d257a7a14bcf83977039c39c0cf7
         applyMiddleware(...middlewares)
+=======
+        applyMiddleware(thunk, logger)
+>>>>>>> add store.js
     )
 );
 
